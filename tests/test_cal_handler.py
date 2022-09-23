c36ab725eb3a4ff8b01e2b195a3942c7acd4ea55
Eliza Wilson <elizaaverywilson@gmail.com>
2022-09-22 21:17:13 -0500

    Added pytest.ini

--- tests/test_cal_handler.py
+++ tests/test_cal_handler.py
@@ -1,16 +1,27 @@
 import datetime
 
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def change_test_dir(request, monkeypatch):
+    monkeypatch.chdir(request.fspath.dirname)
+
+
+# noinspection PyPep8
 from enums import Instrument
+# noinspection PyPep8
 import cal_handler
+# noinspection PyPep8
 from events import StudioEvent
 
 
 class TestCombine:
     start = datetime.datetime(2021, 9, 3)
     delta = datetime.timedelta(hours=1)
-    event1 = StudioEvent(start_time=start, end_time=start+delta, instruments={Instrument.FIDDLE})
-    event2 = StudioEvent(start_time=start+delta, end_time=start+2*delta, instruments={Instrument.VIOLA})
-    event3 = StudioEvent(start_time=start+4*delta, end_time=start+5*delta, instruments={Instrument.VIOLIN})
+    event1 = StudioEvent(start_time=start, end_time=start + delta, instruments={Instrument.FIDDLE})
+    event2 = StudioEvent(start_time=start + delta, end_time=start + 2 * delta, instruments={Instrument.VIOLA})
+    event3 = StudioEvent(start_time=start + 4 * delta, end_time=start + 5 * delta, instruments={Instrument.VIOLIN})
 
     combined_events = cal_handler.combine_adjacent_events([event1, event2, event3])
 
@@ -19,11 +30,11 @@ class TestCombine:
 
     def test_start_time(self):
         assert self.combined_events[0].start_time == self.start
-        assert self.combined_events[1].start_time == self.start+4*self.delta
+        assert self.combined_events[1].start_time == self.start + 4 * self.delta
 
     def test_end_time(self):
-        assert self.combined_events[0].end_time == self.start+2*self.delta
-        assert self.combined_events[1].end_time == self.start+5*self.delta
+        assert self.combined_events[0].end_time == self.start + 2 * self.delta
+        assert self.combined_events[1].end_time == self.start + 5 * self.delta
 
     def test_insrruments(self):
         assert self.combined_events[0].instruments == {Instrument.FIDDLE, Instrument.VIOLA}
