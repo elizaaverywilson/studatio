397fbfa266cc7d94a8f78e611467bad8cd6027a7
Eliza Wilson <elizaaverywilson@gmail.com>
2022-10-02 14:06:00 -0500

    Structured fixtures in tests

--- tests/test_cal_handler.py
+++ tests/test_cal_handler.py
@@ -16,30 +16,44 @@ from studatio import cal_handler
 from studatio.events import StudioEvent
 
 
-class TestCombine:
-    start = datetime.datetime(2021, 9, 3)
-    delta = datetime.timedelta(hours=1)
+@pytest.fixture
+def start():
+    return datetime.datetime(2021, 9, 3)
+
+
+@pytest.fixture
+def delta():
+    return datetime.timedelta(hours=1)
+
+
+@pytest.fixture
+def combined_events(start, delta):
     event1 = StudioEvent(start_time=start, end_time=start + delta, instruments={Instrument.FIDDLE})
     event2 = StudioEvent(start_time=start + delta, end_time=start + 2 * delta, instruments={Instrument.VIOLA})
     event3 = StudioEvent(start_time=start + 4 * delta, end_time=start + 5 * delta, instruments={Instrument.VIOLIN})
 
-    combined_events = cal_handler.combine_adjacent_events([event1, event2, event3])
+    return cal_handler.combine_adjacent_events([event1, event2, event3])
 
-    def test_length(self):
-        assert len(self.combined_events) == 2
 
-    def test_start_time(self):
-        assert self.combined_events[0].start_time == self.start
-        assert self.combined_events[1].start_time == self.start + 4 * self.delta
+def test_length(combined_events):
+    assert len(combined_events) == 2
 
-    def test_end_time(self):
-        assert self.combined_events[0].end_time == self.start + 2 * self.delta
-        assert self.combined_events[1].end_time == self.start + 5 * self.delta
 
-    def test_insrruments(self):
-        assert self.combined_events[0].instruments == {Instrument.FIDDLE, Instrument.VIOLA}
-        assert self.combined_events[1].instruments == {Instrument.VIOLIN}
+def test_start_time(start, delta, combined_events):
+    assert combined_events[0].start_time == start
+    assert combined_events[1].start_time == start + 4 * delta
 
-    def test_plural(self):
-        assert self.combined_events[0].plural is True
-        assert self.combined_events[1].plural is False
+
+def test_end_time(start, delta, combined_events):
+    assert combined_events[0].end_time == start + 2 * delta
+    assert combined_events[1].end_time == start + 5 * delta
+
+
+def test_insrruments(combined_events):
+    assert combined_events[0].instruments == {Instrument.FIDDLE, Instrument.VIOLA}
+    assert combined_events[1].instruments == {Instrument.VIOLIN}
+
+
+def test_plural(combined_events):
+    assert combined_events[0].plural is True
+    assert combined_events[1].plural is False
--- tests/test_events.py
+++ tests/test_events.py
@@ -1,33 +1,47 @@
 import datetime
 
+import pytest
+
 from studatio.enums import Instrument, StudioEventType
 from studatio.events import StudioEvent
 
-start, end = datetime.datetime(2007, 2, 5, 3, 10, 0), datetime.datetime(2007, 2, 5, 4, 10, 0)
+
+@pytest.fixture
+def start():
+    return datetime.datetime(2007, 2, 5, 3, 10, 0)
+
+
+@pytest.fixture
+def end():
+    return datetime.datetime(2007, 2, 5, 4, 10, 0)
 
 
 class TestSimpleEvent:
-    event = StudioEvent(start, end, StudioEventType.LESSON, {Instrument.VIOLA})
+    @pytest.fixture
+    def event(self, start, end):
+        return StudioEvent(start, end, StudioEventType.LESSON, {Instrument.VIOLA})
 
-    def test_date(self):
-        assert 'Feb 05 2007' in str(self.event)
+    def test_date(self, event):
+        assert 'Feb 05 2007' in str(event)
 
-    def test_times(self):
-        assert '03:10 AM to 04:10 AM' in str(self.event)
+    def test_times(self, event):
+        assert '03:10 AM to 04:10 AM' in str(event)
 
-    def test_type(self):
-        assert 'Lesson' in str(self.event)
+    def test_type(self, event):
+        assert 'Lesson' in str(event)
 
-    def test_instrument(self):
-        assert 'Viola' in str(self.event)
+    def test_instrument(self, event):
+        assert 'Viola' in str(event)
 
 
 class TestMultiple:
-    event = StudioEvent(start, end, StudioEventType.LESSON, {Instrument.VIOLIN, Instrument.VIOLA, Instrument.FIDDLE},
-                        True)
+    @pytest.fixture
+    def event(self, start, end):
+        return StudioEvent(start, end, StudioEventType.LESSON, {Instrument.VIOLIN, Instrument.VIOLA,
+                                                                Instrument.FIDDLE}, True)
 
-    def test_multiple_instruments(self):
-        assert 'Violin/Viola/Fiddle' in str(self.event)
+    def test_multiple_instruments(self, event):
+        assert 'Violin/Viola/Fiddle' in str(event)
 
-    def test_plural(self):
-        assert 'Lessons' in str(self.event)
+    def test_plural(self, event):
+        assert 'Lessons' in str(event)
